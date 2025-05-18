8bee00e0f648243a3e9aa1f026e5290cf32c5c85
fwraymond <83242507+fwraymond@users.noreply.github.com>
2025-05-18 12:19:07 -0400

    init server with bin script

--- src/manager.tsx
+++ src/manager.tsx
@@ -5,25 +5,13 @@ import { Tab } from "./components/Tab";
 
 import { ADDON_ID,  TAB_ID } from "./constants";
 
-/**
- * Note: if you want to use JSX in this file, rename it to `manager.tsx`
- * and update the entry prop in tsup.config.ts to use "src/manager.tsx",
- */
-
-// Register the addon
 addons.register(ADDON_ID, (api) => {
-
-  // addons.add(TOOL_ID, {
-  //   type: types.TOOL,
-  //   title: "My addon 2",
-  //   match: ({ tabId }) =>
-  //     !!(tabId === TAB_ID),
-  //   render: () => <Tool api={api} />,
-  // });
 
   addons.add(TAB_ID, {
     type: types.TAB,
     title: "VPAT",
+    // only show on local builds
+    match: () => location.hostname.match(/(localhost|127\.0\.0\.1)/) !== null,
     render: ({ active }) => <Tab active={active} />,
   });
 });
