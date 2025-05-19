40f4a39d7b6f90cb0aa21bcdb83ebcef7f87f10b
fwraymond <83242507+fwraymond@users.noreply.github.com>
2025-05-18 23:19:08 -0400

    trim report contents

--- src/server/createReport.js
+++ src/server/createReport.js
@@ -1,10 +1,21 @@
 import { writeFileSync } from "fs";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { timeStamp } from "console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+function trimResult(result)
+{
+  return {
+    ...result,
+    description: undefined,
+    help: undefined,
+    helpUrl: undefined,
+    nodes: undefined,
+  }
+}
 export function createReport(report)
 {
   const toSave = report.map((result) => {
@@ -12,10 +23,18 @@ export function createReport(report)
       ...result,
       inapplicable: undefined,
       passes: undefined,
+      testEngine: undefined,
+      testEnvironment: undefined,
+      toolOptions: undefined,
+      testRunner: undefined,
+      url:undefined,
+      violations: result.violations.map(trimResult),
+      incomplete: result.incomplete.map(trimResult),
+      timestamp: undefined
     }
   })
 
   const id = new Date().getTime()
-  writeFileSync(`${__dirname}/htdocs/reports/${id}.json`, JSON.stringify(toSave, null, 2), 'utf8');
+  writeFileSync(`${__dirname}/htdocs/reports/${id}.json`, JSON.stringify(toSave, null), 'utf8');
   return id
 }
